7699bfca5601d0af7c65732d4becf4b0a5df6b14
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-10-14 19:36:20 -0400

    add form components for New Todo page

--- src/components/pages/all-todos/index.jsx
+++ src/components/pages/all-todos/index.jsx
@@ -2,23 +2,24 @@ import './styles.css';
 import { Todo } from "../../todo";
 
 export const AllTodosPage = () => {
+  const todos = [
+    { text: 'Walk my dog', date: '10/10/2021', color: 'blue', isComplete: false },
+    { text: 'Buy laundry detergent', date: '11/10/2021', color: 'purple', isComplete: false },
+    { text: 'Workout', date: '12/10/2021', color: 'red', isComplete: true },
+  ];
+
   return (
-    <div>
-      <Todo
-        text="Walk my dog"
-        date="10/10/2021"
-        color="blue"
-      />
-      <Todo
-        text="Buy laundry detergent"
-        date="11/10/2021"
-        color="purple"
-      />
-      <Todo
-        text="Workout"
-        date="12/10/2021"
-        color="red"
-      />
+    <div className="todos-container">
+      {
+        todos.map(todo =>
+          <Todo
+            text={todo.text}
+            date={todo.date}
+            color={todo.color}
+            isComplete={todo.isComplete}
+          />
+        )
+      }
     </div>
   );
 }
